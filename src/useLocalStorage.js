8b6c92fe890f4b3343c4973e04aa2cf416a35064
jswan <jswannieman@gmail.com>
2023-08-24 20:10:09 +1200

    Commit useLocalStorage Javascript Component

---
+++ src/useLocalStorage.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const getValue = (key, defaultValue) => {
+  const storageValue = JSON.parse(localStorage.getItem(key));
+  if (storageValue) {
+    return storageValue;
+  }
+  if (defaultValue instanceof Function) {
+    return defaultValue();
+  }
+  return defaultValue;
+};
+
+const useLocalStorage = ({ key, defaultValue }) => {
+  const [value, setValue] = useState(() => {
+    return getValue(key, defaultValue);
+  });
+
+  useEffect(() => {
+    return localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
