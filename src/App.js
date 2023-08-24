f0ee06e621acada876e09f927028d6d8575dfa39
jswan <jswannieman@gmail.com>
2023-08-24 20:10:27 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import "./App.css";
+import { useState } from "react";
+import useLocalStorage from "./useLocalStorage";
 
 function App() {
-  return <div className="App"></div>;
+  const [sports, setSports] = useLocalStorage("sports", "");
+  const [saveSports, setSaveSports] = useState([]);
+
+  const handleInput = (event) => {
+    event.preventDefault();
+    setSports(event.target.value);
+    setSaveSports([...saveSports, event.target.value]);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <form>
+          <label>Enter a Sport : </label>
+          <input
+            type="text"
+            defaultValue={sports}
+            autoFocus
+            onBlur={handleInput}
+          />
+        </form>
+        <ul>
+          {saveSports.map((sport, index) => (
+            <li key={index}>{sport}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
