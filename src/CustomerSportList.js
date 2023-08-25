a8ed359ce6a138e13acaaa189a6df5d7ffe09823
jswan <jswannieman@gmail.com>
2023-08-25 16:25:57 +1200

    Commit CustomerSportList Javascript Component

---
+++ src/CustomerSportList.js
@@ -0,0 +1,24 @@
+const CustomerSportList = ({ setShowForm }) => {
+  const custSportList = JSON.parse(localStorage.getItem("sports"));
+
+  return (
+    <>
+      <div className="cust-container">
+        <h2>Your List of Sports</h2>
+        <button
+          id="cust-button2"
+          onClick={() => setShowForm((prevShowForm) => !prevShowForm)}
+        >
+          Back to Form
+        </button>
+      </div>
+      <ul className="cust__sport-list">
+        {custSportList.map((sport, index) => (
+          <li key={index}>{sport}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default CustomerSportList;
